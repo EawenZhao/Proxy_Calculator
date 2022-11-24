615378c1fec1c27ec08ddce768d9a4f0cb73ebdc
parala <zyh1999zyh@yeah.net>
2022-11-24 16:06:16 +0800

    Initial commit

---
+++ src/AddUnit.java
@@ -0,0 +1,11 @@
+public class AddUnit implements CalcUnit{
+    @Override
+    public boolean fit(String operator) {
+        return operator.contains("+");
+    }
+
+    @Override
+    public double calc(double x, double y) {
+        return x + y;
+    }
+}
---
+++ src/CalcUnitProxy.java
@@ -0,0 +1,10 @@
+import java.io.PrintWriter;
+import java.lang.reflect.Proxy;
+
+public class CalcUnitProxy {
+    CalcUnit calcUnit;
+
+    Proxy proxy
+    public void setLogWriter(PrintWriter pw) {
+    }
+}
